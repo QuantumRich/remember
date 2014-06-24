6fa15f6cdf0b448175e4b78bdbb12b5f7d8b8f4b
Bryce Sperling <bryce.sperling@ge.com>
2014-06-24 17:01:01 -0400

    Added console logs of all images to be shown. Edited js and event.html

--- public/js/eventpagecontroller.js
+++ public/js/eventpagecontroller.js
@@ -2,10 +2,16 @@ $(function() {
 
 	var eventCode = $.url().param('code');
 	
-	//function showPhoto
+	function showPhotos(photos) {
+		//var temp = "<div class='cell' style='width:{width}px; height: {height}px; background-image: url(i/photo/{index}.jpg)'></div>";
+		$.each(photos, function(i,photos) {
+			console.log(photos);
+		})
+	}
 	
 	function loadEvent(data) {
-		console.log(data)
+		showPhotos(data.pics);
+		//console.log(data)	//Put append/show pics method
 		$(document).on('change', '.btn-file :file', function() {
 			  var input = $(this),
 			      numFiles = input.get(0).files ? input.get(0).files.length : 1,
@@ -28,12 +34,12 @@ $(function() {
 	            	data.append("picture" + i, file);
 	            });
 		            
-		        if( input.length ) {
+	            /*if( input.length ) {
 		            input.val(log);
 		            
 		        } else {
 		            if( log ) console.log(log);
-		        }
+		        }*/
 		        $.ajax({
 		        	  type: "POST",
 		        	  url: "/api/upload/" + eventCode,
@@ -41,7 +47,9 @@ $(function() {
 		        	  processData: false,
 		              contentType: false,
 		        	  success: function(uploadedPics) {
-		        		  console.log(uploadedPics);
+		        		  //console.log(uploadedPics);
+		        		  //put append/show pics method here.
+		        		  showPhotos(uploadedPics);
 		        		  
 		        	  }
 		        	  //dataType: dataType
